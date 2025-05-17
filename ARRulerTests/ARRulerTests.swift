72c4bf4dd96efb4e7227b812501651cfb1279905
jscheth <sovichea.chet@gmail.com>
2025-05-16 22:06:31 -0700

    Initial Commit

---
+++ ARRulerTests/ARRulerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ARRulerTests.swift
+//  ARRulerTests
+//
+//  Created by Jonathan Cheth on 5/16/25.
+//
+
+import Testing
+@testable import ARRuler
+
+struct ARRulerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
